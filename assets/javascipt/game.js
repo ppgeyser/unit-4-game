f0ededd249079768d7f1243cf5b191ac06cb46ac
Peter Geyser <peterpgeyser@gmail.com>
2019-07-19 15:45:50 -0700

    Game win functionality added. Partially working rest functionality.

--- assets/javascipt/game.js
+++ assets/javascipt/game.js
@@ -7,7 +7,6 @@ $(document).ready(function () {
     $("#intro-button").click(function(){
         $("#intro").hide();
         $("#content").show();
-        generateCrystals()
 
         var wins = 0;
         var losses = 0;
@@ -21,14 +20,13 @@ $(document).ready(function () {
         ];
 
         //Print wins, losses, crystalscore, and userscore
-        $('#crystal-score-text').text(crystalScore);
         $('#user-score-text').text(userScore);
         $('#wins-text').text(wins);
         $('#losses-text').text(losses);
 
         //generate random number between 19 and 120 for crystal score
         var crystalScore = Math.floor(Math.random()*(102)+19);
-        console.log(crystalScore);
+        $('#crystal-score-text').text(crystalScore);
 
         //randomize values for each of the four crystals
         function generateCrystals () {
@@ -43,7 +41,6 @@ $(document).ready(function () {
 
                 //add path stored in array
                 Crystal.attr("src", crystalPath[i]);
-                console.log(crystalPath[i]);
 
                 //add random crystal value between 1 and 12
                 //Math.floor(Math.random()*(max-min+1)+min); min: 1, max: 12
@@ -55,27 +52,71 @@ $(document).ready(function () {
               }
             }
 
-        
+        generateCrystals()
+
+
         //reset function
+        function reset () {
             //userscore = 0
+            userScore = 0;
+
             //randomize crystal score
+            crystalScore = Math.floor(Math.random()*(102)+19);
+
+            //clear pre-existing crystals
+            $('#crystals').html('');
+
+            //reprint reset values
+            $('#user-score-text').text(userScore);
+            $('#wins-text').text(wins);
+            $('#losses-text').text(losses);
+            $('#crystal-score-text').text(crystalScore);
+
             //randomize crystal values
-
+            generateCrystals ();
+        }
+            
         //crystal onclick
-            //add crystal value to userscore
+        $(".crystal-image").on("click", function() {
 
-        //if crystal score === userscore
-            //show victory screen
-            //wins++
-            //play again onclick
-                //reset function, show content screen
+            //add crystal value to userscore
+            var crystalValue = ($(this).attr("crystalValue"));
+            crystalValue = parseInt(crystalValue);
+            userScore += crystalValue;
+            $('#user-score-text').text(userScore);
+            console.log(userScore);
+
+            //if crystal score === userscore
+            if (crystalScore === userScore) {
+
+                //show victory screen
+                $("#content").hide();
+                $('#victory').show();
+
+                //wins++
+                wins++;
+
+                //play again onclick
+                $("#win-button").on("click", function () {
+        
+                    //reset function, show content screen
+                    $('#victory').hide();
+                    $("#content").show();
+                    reset ();
+                })
+                    
+            }
+
+        });
+
+        
 
         //if crystal score < userscore
             //show defeat screen
             //losses++
             //play again onclick
                 //reset function, show content screen
-
+        
       });
     
 });
